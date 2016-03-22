1d7301efe67dd354b926818080f338b9e7154ac5
liuruibin <iceraining@yeah.net>
2016-03-22 16:21:50 +0800

    测试指令的scope 和this

--- code/029-自定义指令 controller && controllAs属性/app/index.js
+++ code/029-自定义指令 controller && controllAs属性/app/index.js
@@ -3,30 +3,39 @@ angular.module('myApp', [])
     .directive('bookList', function () {
         return {
             restrict: 'ECAM',
-            controller: function ($scope) {
+            controller: ['$scope', function ($scope) {
                 $scope.books = [
                     {
                         name: 'php'
-                    },
-                    {
+                    }, {
                         name: 'javascript'
-                    },
-                    {
+                    }, {
                         name: 'java'
                     }
                 ];
-                $scope.addBook = function(){
+                /**
+                 * 如果这里写$scope.addBook
+                 * 那么调用的时候用scope.addBook
+                 */
+                $scope.addBook = function () {
+                    console.log('scope')
+                };
 
-                }
-                this.addBook = function(){
+                /**
+                 * 如果用this.addBook
+                 * 调用的时候使用bookListController.addBook
+                 */
+                this.addBook = function () {
                     // ...
-                }
-            },
-            controllerAs:'bookListController',
+                    console.log('this')
+                };
+            }],
+            controllerAs: 'bookListController',
             template: '<ul><li ng-repeat="book in books">{{book.name}}</li></ul>',
-            replace:true,
-            link:function(scope,iEelement,iAttrs,bookListController){
-                iEelement.on('click',bookListController.addBook)
+            replace: true,
+            link: function (scope, element, attrs, bookListController) {
+                // 绑定事件
+                element.on('click', bookListController.addBook)
             }
         }
 
