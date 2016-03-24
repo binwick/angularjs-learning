ce10e70776c165a27c792dba913a8ddfc4b37f87
liuruibin <iceraining@yeah.net>
2016-03-24 10:05:53 +0800

    自定义指令

--- code/026-自定义指令 templateUrl属性/app/index.js
+++ code/026-自定义指令 templateUrl属性/app/index.js
@@ -7,7 +7,6 @@ var myApp = angular.module('myApp', [])
             replace: true
         }
     })
-
     .directive('customTags2', function () {
         return {
             restrict: 'ECAM',
@@ -18,6 +17,15 @@ var myApp = angular.module('myApp', [])
 
     .controller('firstController', ['$scope', '$compile', function ($scope, $compile) {
         $scope.name = '张三';
-
-        angular.element('.container').append($compile('<custom-tags></custom-tags>')($scope));
+        var top = [
+            {
+                id: '001',
+                content: '<custom-tags></custom-tags>'
+            }, {
+                id: '002',
+                content: '<custom-tags2></custom-tags2>'
+            }
+        ];
+        angular.element('.container').append($compile(top[0]['content'])($scope));
+        angular.element('.container').append($compile(top[1]['content'])($scope));
     }]);
