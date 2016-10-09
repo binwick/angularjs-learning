16b35f5e805c50fef2cb007ce3f191413ac2d24a
liuruibin <iceraining@yeah.net>
2016-10-09 13:06:15 +0800

    bug fix

--- code/042-面试js/app/index.js
+++ code/042-面试js/app/index.js
@@ -18,10 +18,27 @@ function getName() {
 }
 
 //请写出以下输出结果：
-Foo.getName(); // 2
-getName(); // 5 4
-Foo().getName(); // 3 1
-getName(); // 5 1
-new Foo.getName(); // 2
-new Foo().getName(); // 3
-new new Foo().getName(); //3
+// Foo.getName(); // 2
+// getName(); // 5 4
+// Foo().getName(); // 3 1
+// getName(); // 5 1
+// new Foo.getName(); // 2
+// new Foo().getName(); // 3
+// new new Foo().getName(); //3
+
+
+(function () {
+    console.log("hello world 1 ...");
+    return false; // logs false
+}());
+
+
+(function () {
+    console.log("hello world 2 ...");
+    return false; // Also returns false
+})();
+
+
+
+
+
