70dc9cc44466763742014cbe1916f545e7e01da7
winiki <winiki.liu@gmail.com>
2017-03-27 21:09:53 +0800

    'upload bug info - 1490620193.4'

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def file_handle():
     check_file()
     git_pull()
     add_file_line()
-    time.sleep(random.randint(0, 360))  # overtrue 所说的情怀 不需要可注释
+    # time.sleep(random.randint(0, 360))  # overtrue 所说的情怀 不需要可注释
     # cmd_go()
     git_add()
     git_commit()
