4f2f5a20a0cde31021019f1109da17286d248c77
binwick <winiki.liu@gmail.com>
2018-02-24 22:56:29 +0800

    some code

--- code/042-面试js/app/index.js
+++ code/042-面试js/app/index.js
@@ -4,6 +4,7 @@ function Foo() {
     };
     return this;
 }
+
 Foo.getName = function () {
     alert(2);
 };
@@ -13,6 +14,7 @@ Foo.prototype.getName = function () {
 var getName = function () {
     alert(4);
 };
+
 function getName() {
     alert(5);
 }
@@ -39,6 +41,63 @@ function getName() {
 })();
 
 
+function foo(p1, p2) {
+    this.val = p1 + p2;
+}
 
+var bar = foo.bind(null, "p1"); // null -> this, p1 -> 下层函数
+console.log(bar);
+var baz = new bar("p2");
+console.log(baz.val); //  p1p2
+
+// 被忽略的this
+function foo() {
+    console.log(this.a)
+}
+
+var a = 2;
+foo.call(null); // 2
+
+//
+function foo(a, b) {
+    console.log("a:" + a, "b:" + b);
+}
+
+foo.apply(null, [2, 3]);
+var bar = foo.bind(null, 2);
+bar(3);
+
+// 软绑定
+if (!Function.prototype.softBind) {
+    Function.prototype.softBind = function (obj) {
+        var fn = this,
+            curried = [].slice.call(arguments, 1),
+            bound = function bound() {
+                return fn.apply(
+                    (!this ||
+                        (typeof window !== "undefined" &&
+                            this === window) ||
+                        (typeof global !== "undefined" &&
+                            this === global)
+                    ) ? obj : this,
+                    curried.concat.apply(curried, arguments)
+                );
+            };
+        bound.prototype = Object.create(fn.prototype);
+        return bound;
+    };
+}
+
+function foo() {
+    console.log("name:" + this.name);
+}
+
+var obj = {name: "obj"}, obj2 = {name: "obj2"}, obj3 = {name: "obj3"};
+var fooOBJ = foo.softBind(obj);
+fooOBJ();// obj
+obj2.foo = foo.softBind(obj);
+obj2.foo(); // obj2
+fooOBJ.call(obj3); // obj3
+setTimeout(obj2.foo, 10);// obj
 
 
